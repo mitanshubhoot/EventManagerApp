55b1da65e9b4269c5cdfc9f1b12e50d1f2b03817
mr-jir <jitendranv2000@gmail.com>
2020-05-01 02:15:36 +0530

    some changes

--- app/src/main/java/com/example/event_manager_app/AddParticipantActivity.java
+++ app/src/main/java/com/example/event_manager_app/AddParticipantActivity.java
@@ -36,7 +36,7 @@ public class AddParticipantActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if(!email.getText().toString().equals("") && !event_code.getText().toString().equals("")) {
                     pmodel.addParticipant(name.getText().toString(), event_code.getText().toString(),college.getText().toString(),year.getText().toString() , email.getText().toString(), Long.parseLong(contact.getText().toString()));
-                    Toast.makeText(getBaseContext(), "participant added ", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getBaseContext(), "participant successful added ", Toast.LENGTH_SHORT).show();
                 }
             }
         });
