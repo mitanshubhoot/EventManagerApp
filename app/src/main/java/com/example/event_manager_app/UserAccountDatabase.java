077ed08bd80070989ef0a75a1381207b3146774b
mr-jir <jitendranv2000@gmail.com>
2020-05-02 20:11:06 +0530

    updated user database

--- app/src/main/java/com/example/event_manager_app/UserAccountDatabase.java
+++ app/src/main/java/com/example/event_manager_app/UserAccountDatabase.java
@@ -6,12 +6,16 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+
 @Database(entities = {UserAccount.class}, version = 1 )
+
 
 public abstract class UserAccountDatabase extends RoomDatabase {
 
+
     public abstract UserAccountDao userAccountDao();
     public static UserAccountDatabase INSTANCE;
+
 
     public static UserAccountDatabase getAppDatabase(Context context)
     {
@@ -24,6 +28,7 @@ public abstract class UserAccountDatabase extends RoomDatabase {
         return INSTANCE;
 
     }
+
 
     public static void destroyInstance()
     {
